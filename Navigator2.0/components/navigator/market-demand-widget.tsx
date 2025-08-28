c4a33807b5f4f928689f19e1a7fd6d3fd084f101
kuldeeprategain <kuldeep.gupta@rategain.com>
2025-08-28 19:34:48 +0530

    Event Fix on Overview

--- Navigator2.0/components/navigator/market-demand-widget.tsx
+++ Navigator2.0/components/navigator/market-demand-widget.tsx
@@ -142,8 +142,9 @@ export function MarketDemandWidget() {
     }
     getAllSubscribeEvents(payload)
       .then((res) => {
+        debugger
         if (res.status && res.body && res.body.eventDetails) {
-          res.body.eventDetails.sort((a: any, b: any) => a.rowNum - b.rowNum).fillter((x: any) => x.isSubscribed === true);
+          res.body.eventDetails.sort((a: any, b: any) => a.rowNum - b.rowNum).filter((x: any) => x.isSubscribed === true);
           setEventData(res.body?.eventDetails);
           // setinclusionValues(res.body.map((inclusion: any) => ({ id: inclusion, label: inclusion })));
         }
