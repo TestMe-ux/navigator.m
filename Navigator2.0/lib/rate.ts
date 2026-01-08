1c2e5bf0c3fae67f8808709e884077810d3d01dc
RahulKumar-UXD <rahul.kumar@rategain.com>
2026-01-08 12:29:20 +0530

    Exclude Navigator2.0 from repository, keep only Mobile-Version

--- Navigator2.0/lib/rate.ts
+++
@@ -1,7 +0,0 @@
-import apiClient from "./client";
-import { Constants } from "./constants";
-
-export async function getRateTrends(filtersValue: any) {
-    const { data } = await apiClient.post(`${Constants.API_GetRateTrend}`, filtersValue);
-    return data;
-}
